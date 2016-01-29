ce77bd2204f90fbc52010f85bfbea9c7cbfde7ca
César Madrid <cmadrid@espol.edu.ec>
2016-01-29 12:55:36 -0500

    tomado de fotos compartidas desde otros lados. y nueva interfaz luego de tomar la foto.

--- app/src/main/java/ec/edu/espol/integradora/dadtime/CameraActivity.java
+++ app/src/main/java/ec/edu/espol/integradora/dadtime/CameraActivity.java
@@ -1,35 +1,32 @@
 package ec.edu.espol.integradora.dadtime;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.content.res.ColorStateList;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.media.ExifInterface;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -41,72 +38,108 @@ import java.util.Set;
  * Created by ces_m
  * on 1/5/2016.
  */
-public class CameraActivity extends AppCompatActivity {
-    private ImageView test;
-    private Button button;
+public class CameraActivity extends Activity {
     private String CurrentPhotoPath;
     private Uri CapturedImageURI;
-    private LinearLayout tags;
 
     private SharedPreferences preferenceSettings;
-    private SharedPreferences.Editor preferenceEditor;
     private int REQUEST_TAKE_PHOTO = 102;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera_image_tag);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String action = intent.getAction();
+
+        if (Intent.ACTION_SEND.equals(action)) {
+            if (extras.containsKey(Intent.EXTRA_STREAM)) {
+                Uri uri = (Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM);
+                Bitmap bitmap=null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                    System.out.println(uri);
+                    getImageShared(bitmap);
 
 
-        if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY) != 0) {
-            this.finish();
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        }
+                    System.out.println(uri.getPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-        //getSupportActionBar().hide();
-
-
-        View mControlsView = findViewById(R.id.fullscreen_content_controls);
-        View mContentView = findViewById(R.id.fullscreen_content);
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | (Build.VERSION.SDK_INT > 15 ? SYSTEM_UI_FLAG_IMMERSIVE_STICKY() : 0)
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-
-        mControlsView.setVisibility(View.VISIBLE);
-
-
-        test = (ImageView) findViewById(R.id.image);
-        tags = (LinearLayout) findViewById(R.id.tags);
-        button = (Button) findViewById(R.id.dummy_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addMetadataGallery(CurrentPhotoPath, getApplicationContext());
-                finish();
+                return;
             }
-        });
+        }
 
         dispatchTakePictureIntent();
 
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-        getActivities();
     }
 
-    @TargetApi(Build.VERSION_CODES.KITKAT)
-    public int SYSTEM_UI_FLAG_IMMERSIVE_STICKY() {
-        return View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+    public void getImageShared(final Bitmap bitmap){
+
+        final Context ctx = this;
+        LayoutInflater inflater = this.getLayoutInflater();
+        View view = inflater.inflate(R.layout.post_camera, null);
+        ImageView imageCam = (ImageView)view.findViewById(R.id.imageCam);
+        imageCam.setImageBitmap(bitmap);
+        imageCam.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        Spinner s = (Spinner) view.findViewById(R.id.listActivities);
+        String[] array =  getActivities();
+        ArrayAdapter<String> adapter;
+        if(array!=null && array.length>0 )
+            adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,array);
+        else
+            adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,new String[]{});
+        s.setAdapter(adapter);
+        new AlertDialog.Builder(this).setView(view)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            saveFile(bitmap);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        //writeTag("tagSeleccionadoShared");
+                        finish();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setCancelable(false)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 
+    protected void saveFile(Bitmap bmScreen2) {
+        // TODO Auto-generated method stub
+
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "/DadTime/SHARED_" + timeStamp + ".jpg";
+        new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES)+"/DadTime/").mkdirs();
+
+        // String fname = "Upload.png";
+        File saved_image_file = new File(
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES)
+                        + imageFileName);
+        try {
+            FileOutputStream out = new FileOutputStream(saved_image_file);
+            bmScreen2.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        addImageToGallery(saved_image_file.getAbsolutePath(), getApplicationContext());
+
+    }
 
     protected void dispatchTakePictureIntent() {
 
@@ -124,27 +157,34 @@ public class CameraActivity extends AppCompatActivity {
 
         CameraActivity activity = (CameraActivity) context;
         if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
-
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                ex.printStackTrace();
-                Toast toast = Toast.makeText(activity, "Error guardando la foto.", Toast.LENGTH_SHORT);
-                toast.show();
-                finish();
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri fileUri = Uri.fromFile(photoFile);
-                CapturedImageURI = fileUri;
-                CurrentPhotoPath = fileUri.getPath();
+            if (createImageProcess(activity)) {
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
                         CapturedImageURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
+    }
+
+    public boolean createImageProcess(Context ctx){
+
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            ex.printStackTrace();
+            Toast toast = Toast.makeText(ctx, "Error guardando la foto.", Toast.LENGTH_SHORT);
+            toast.show();
+            finish();
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri fileUri = Uri.fromFile(photoFile);
+            CapturedImageURI = fileUri;
+            CurrentPhotoPath = fileUri.getPath();
+            return true;
+        }
+        return false;
     }
 
     protected File createImageFile() throws IOException {
@@ -164,12 +204,45 @@ public class CameraActivity extends AppCompatActivity {
         return image;
     }
 
+    private void getTagString(String tag, ExifInterface exif)
+    {
+        System.out.println(tag + " : " + exif.getAttribute(tag) + "\n");
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        System.out.println("request: "+requestCode);
+        System.out.println("request: " + requestCode);
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
             add();
-            setBackground(CurrentPhotoPath);
+            LayoutInflater inflater = this.getLayoutInflater();
+            View view = inflater.inflate(R.layout.post_camera, null);
+            ImageView imageCam = (ImageView)view.findViewById(R.id.imageCam);
+            imageCam.setImageBitmap(Collage.getSmallBitmap(CurrentPhotoPath));
+            imageCam.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            Spinner s = (Spinner) view.findViewById(R.id.listActivities);
+            String[] array =  getActivities();
+            ArrayAdapter<String> adapter;
+            if(array!=null && array.length>0 )
+                adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,array);
+            else
+                adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,new String[]{});
+            s.setAdapter(adapter);
+
+            new AlertDialog.Builder(this).setView(view)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setCancelable(false)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+
         } else {
             File tmp = new File(CurrentPhotoPath);
             if (tmp.exists()) tmp.delete();
@@ -181,6 +254,21 @@ public class CameraActivity extends AppCompatActivity {
 
     }
 
+    public void writeTag(String tag){
+        try {
+            ExifInterface exif = new ExifInterface(CurrentPhotoPath);
+            System.out.println(CurrentPhotoPath);
+            exif.setAttribute(ExifInterface.TAG_MAKE, "DadTime");
+            exif.setAttribute(ExifInterface.TAG_MODEL, tag);
+            getTagString(ExifInterface.TAG_MAKE, exif);
+            getTagString(ExifInterface.TAG_MODEL, exif);
+            exif.saveAttributes();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     protected void add() {
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File f = new File(CurrentPhotoPath);
@@ -189,84 +277,30 @@ public class CameraActivity extends AppCompatActivity {
         sendBroadcast(mediaScanIntent);
     }
 
-    private void setBackground(String imagePath) {
-
-        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
-        test.setImageBitmap(bitmap);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Camera Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://ec.edu.espol.integradora.dadtime/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Camera Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://ec.edu.espol.integradora.dadtime/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
-    public void getActivities(){
+    public String[] getActivities(){
         preferenceSettings = getSharedPreferences("myPreferences", Context.MODE_PRIVATE);
-        preferenceEditor = preferenceSettings.edit();
         Set<String> getSetTitleEntertainments = preferenceSettings.getStringSet("titleEntertainments", null);
 //Si es diferente de null tiene actividades seleccionadas, eso debes validar q no puede abrir nuestra camara si no existen actividades....
         if (getSetTitleEntertainments != null)
         {
             //En este array estan los títulos de actividades seleccionadas, es decir debes ponerlas en donde tenías los checks después de tomar la foto...
             ArrayList<String> titleEntertainments = new ArrayList<String>(getSetTitleEntertainments);
-            for(String actividad:titleEntertainments){
-                CheckBox cb = new CheckBox(getApplicationContext());
-                cb.setText(actividad);
-                cb.setTextColor(Color.WHITE);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    cb.setButtonTintList(ColorStateList.valueOf(Color.WHITE));
-                }else
-                {
-                    int id = Resources.getSystem().getIdentifier("btn_check_holo_dark", "drawable", "android");
-                    cb.setButtonDrawable(id);
-                }
-                tags.addView(cb);
-            }
+
+            String[] titleEntertainmentsArray = new String[titleEntertainments.size()];
+            titleEntertainmentsArray = titleEntertainments.toArray(titleEntertainmentsArray);
+            return titleEntertainmentsArray;
         }
+        return null;
     }
-    public static void addMetadataGallery(final String filePath, final Context context) {
+    public static void addImageToGallery(final String filePath, final Context context) {
 
         ContentValues values = new ContentValues();
 
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
         values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
         values.put(MediaStore.MediaColumns.DATA, filePath);
-        values.put(MediaStore.MediaColumns.TITLE, "tipo");
-
 
         context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
     }
 }
